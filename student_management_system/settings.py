4775a08633f0c88a4a66cf0f7dab3bcf6a46ee40
VanTrietTRAN <work.triettran@gmail.com>
2025-10-20 18:24:49 +0700

    staff login test

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -29,6 +29,12 @@ DEBUG = True
 #ALLOWED_HOSTS = ["studentmanagementsystem22.herokuapp.com"]
 ALLOWED_HOSTS = ["*"]
 
+# CSRF Settings
+CSRF_TRUSTED_ORIGINS = [
+    'http://127.0.0.1:8000',
+    'http://localhost:8000',
+]
+
 MEDIA_URL="/media/"
 MEDIA_ROOT=os.path.join(BASE_DIR,"media")
 
